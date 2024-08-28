7e12503808968b8a8665995647966cb72c66abff
Amit <amit.stp2001@gmail.com>
2024-08-28 17:04:47 +0000

    final

--- appbackup.js
+++ appbackup.js
@@ -1,13 +1,18 @@
+
+
 const countdown = () => {
-    const endDate = new Date("Aug 29, 2024 00:00:00").getTime();
+    const endDate = new Date("aug 29, 2024 00:00:00").getTime();
     const now = new Date().getTime();
+
     const difference = endDate - now;
 
     const seconds = 1000;
     const minutes = seconds * 60;
     const hours = minutes * 60;
+    const days = hours * 24;
 
-    let timeHours = Math.floor(difference / hours);
+    let timeDays = Math.floor(difference / days);
+    let timeHours = Math.floor((difference % days) / hours);
     let timeMinutes = Math.floor((difference % hours) / minutes);
     let timeSeconds = Math.floor((difference % minutes) / seconds);
 
@@ -15,13 +20,12 @@ const countdown = () => {
     timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes;
     timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds;
 
+    document.getElementById("days").innerHTML = timeDays;
     document.getElementById("hours").innerHTML = timeHours;
     document.getElementById("minutes").innerHTML = timeMinutes;
     document.getElementById("seconds").innerHTML = timeSeconds;
+
+   
+
 }
-
-// Initial call to set the values immediately
-countdown();
-
-// Update every second
 setInterval(countdown, 1000);
