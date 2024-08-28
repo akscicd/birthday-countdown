81d4b719a6613dc4d55930f6b0aece1eb1c31075
Amit Kumar Shukla <110124443+akscicd@users.noreply.github.com>
2024-08-28 21:35:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 const countdown = () => {
-    const endDate = new Date("aug 29, 2024 00:00:00").getTime();
+    const endDate = new Date("may 8, 2023 00:00:00").getTime();
     const now = new Date().getTime();
 
     const difference = endDate - now;
@@ -9,7 +9,9 @@ const countdown = () => {
     const seconds = 1000;
     const minutes = seconds * 60;
     const hours = minutes * 60;
+    const days = hours * 24;
 
+    let timeDays = Math.floor(difference / days);
     let timeHours = Math.floor((difference % days) / hours);
     let timeMinutes = Math.floor((difference % hours) / minutes);
     let timeSeconds = Math.floor((difference % minutes) / seconds);
@@ -18,6 +20,7 @@ const countdown = () => {
     timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes;
     timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds;
 
+    document.getElementById("days").innerHTML = timeDays;
     document.getElementById("hours").innerHTML = timeHours;
     document.getElementById("minutes").innerHTML = timeMinutes;
     document.getElementById("seconds").innerHTML = timeSeconds;
